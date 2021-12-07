3abc269c7bb87ea8bbdf5984bdc553d5f4e29eb4
Ivan Higa <ivzeq.higa@gmail.com>
2021-12-07 00:46:47 -0300

    Flexbox grid finished

--- script.js
+++ script.js
@@ -1,59 +1,26 @@
-function createFlexDiv(rowsxcolumns, side){
-    const flexContainer = document.createElement('div');
-    flexContainer.setAttribute('id', 'flexContainer');
-    container.appendChild(flexContainer)
-
-    flexContainer.style.display = 'flex';
-    flexContainer.style.flex = '1'; 
-    flexContainer.style.justifyContent = 'center';
+function createFlexDiv(rowsxcolumns){
+    container.style.display = 'flex';
+    container.style.flexDirection = 'column'
+    container.style.height = '60vh'
+    container.style.width = '60vh'
 
     for (let rowCounter = 0; rowCounter < rowsxcolumns; rowCounter++) {
         const row = document.createElement('div');
         row.classList.add('row');
-        flexContainer.appendChild(row)
+        row.style.display = 'flex';
+        row.style.flex = '1';
+        container.appendChild(row)
         
         for (let columnCounter = 0; columnCounter < rowsxcolumns; columnCounter++) {
             const div = document.createElement('div');
             div.classList.add('square-div');
             row.appendChild(div);
-            div.style.width = side +'px';
-            div.style.height = side +'px';
-            div.style.border = '1px solid red'
+            div.style.border = '1px solid red'  
+            div.style.flex = '1';
         }
-
     }
 }
 
-
-function createGridDiv(rowsxcolumns, side){
-    const gridContainer = document.createElement('div');
-    gridContainer.setAttribute('id', 'gridContainer');
-    container.appendChild(gridContainer);
-    gridContainer.style.display = 'grid';
-
-    for (let columnCounter = 0; columnCounter < rowsxcolumns; columnCounter++) {
-        const div = document.createElement('div');
-        div.classList.add('square-div');
-        gridContainer.appendChild(div);
-        gridContainer.style.gridGap = '0px';
-
-
-        div.style.width = side +'px';
-        div.style.height = side +'px';
-        div.style.border = '1px solid red'
-    }
-}   
-
-const body = document.querySelector('body');
 const container = document.querySelector('#general-container');
 
-body.style.minWidth = '100vh';
-body.style.minHeight = '100vh';
-body.style.display = 'flex';
-body.style.alignItems = 'center'
-
-container.style.display = 'flex';
-container.style.flex = '1';
-container.style.justifyContent = 'center';
-
-createFlexDiv(16,10);
+createFlexDiv(16);
